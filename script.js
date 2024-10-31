ae34edfc2e07f4074c2896606f766c3d1e797db3
angelogpaixao <134895715+angelogpaixao@users.noreply.github.com>
2024-10-31 14:27:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -254,3 +254,11 @@ document.getElementById('cancel-edit-button').addEventListener('click', function
 
 // Chama a função para preencher o formulário se a Página 1 estiver sendo carregada
 window.onload = prefillEditForm;
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
